5e13c68a5dcb8ccd267355eda4ad1cef41b76579
capitangaga <kirillgolowko@yandex.ru>
2018-01-22 11:58:43 +0300

    Add params

--- vacls.client/src/main/java/ru/lumberjackcode/vacls/client/main/Main.java
+++ vacls.client/src/main/java/ru/lumberjackcode/vacls/client/main/Main.java
@@ -2,13 +2,46 @@ package ru.lumberjackcode.vacls.client.main;
 
 
 import org.apache.log4j.Logger;
+import ru.lumberjackcode.vacls.client.applicationparams.VaclsClientParams;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
 
 public class Main {
 
     private final static Logger log = Logger.getLogger(Main.class);
 
-    public static void main(String[] args){
+    private static Unmarshaller paramsUnmarshaller;
+
+    private static VaclsClientParams applicationParams;
+
+    static {
+        try {
+            JAXBContext context = JAXBContext.newInstance(VaclsClientParams.class);
+            paramsUnmarshaller = context.createUnmarshaller();
+        } catch (JAXBException e){
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        log.info("Starting Vacls client app");
+
         System.out.println("Hello, world!");
-        log.debug("Main OK");
+
+        if(args.length != 1){
+            log.error("App takes 1 param: params.xml path");
+            System.exit(1);
+        }
+
+        JAXBElement<VaclsClientParams> root = paramsUnmarshaller.unmarshal(new StreamSource(new File(args[0])), VaclsClientParams.class);
+
+        VaclsClientParams params = root.getValue();
+
+        log.info("Main OK");
     }
 }
