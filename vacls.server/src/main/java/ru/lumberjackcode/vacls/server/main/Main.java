67dc393db4b625d6a2b23a3793ac30d2385b9788
Fox0117 <andrew.konchagin@gmail.com>
2018-01-22 11:18:56 +0300

    Added server module

---
+++ vacls.server/src/main/java/ru/lumberjackcode/vacls/server/main/Main.java
@@ -0,0 +1,12 @@
+package ru.lumberjackcode.vacls.server.main;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    private final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args){
+        System.out.println("Server Main main() OK");
+        logger.debug("Server Main main() OK");
+    }
+}
