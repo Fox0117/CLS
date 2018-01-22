04da7b0b85e24e2286b35a223ddf0f2020c8f0f7
capitangaga <kirillgolowko@yandex.ru>
2018-01-22 10:23:44 +0300

    Add maven client module + root project

---
+++ vacls.client/src/main/java/ru/lumberjackcode/vacls/client/main/Main.java
@@ -0,0 +1,14 @@
+package ru.lumberjackcode.vacls.client.main;
+
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private final static Logger log = Logger.getLogger(Main.class);
+
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+        log.debug("Main OK");
+    }
+}
