751c8f4ef6858ee284899fbc5c9ac1725506eeaf
Fox0117 <andrew.konchagin@gmail.com>
2018-01-30 01:25:08 +0300

    Added face authentication module

---
+++ vacls.server/src/main/java/ru/lumberjackcode/vacls/server/authentication/FaceAuthenticatior.java
@@ -0,0 +1,10 @@
+package ru.lumberjackcode.vacls.server.authentication;
+
+import org.opencv.core.Mat;
+import ru.lumberjackcode.vacls.transfere.ClientRequest;
+
+public class FaceAuthenticatior {
+    public FaceAuthenticatior () {}
+    public boolean Authentificate(ClientRequest clientRequest) { return false; }
+    public String getMessage() { return "Empty"; }
+}
